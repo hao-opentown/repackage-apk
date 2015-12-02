6ed481eaea40d419ec0c84fdce63d27c5a2a13c0
zhihao wang <hao@opentown.cn>
2015-12-02 16:35:13 +0800

    code tiny

--- run.sh
+++ run.sh
@@ -1,13 +1,53 @@
+#!/bin/bash
 
-DEV_HOME=.
-$ANDROID_HOME/build-tools/23.0.2/aapt \
-                        package \
-                        -v \
-                        -f \
-                        -m \
-                        -M $DEV_HOME/AndroidManifest.xml \
-                        -I $ANDROID_HOME/platforms/android-23/android.jar \
-                        -I $DEV_HOME/weixin_660.apk \
-                        -F dist/dist.apk
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $CURRENT_DIR
+
+./clean.sh && mkdir dist
+
+export PATH=$PATH:$ANDROID_HOME/build-tools/23.0.2
+
+
+aapt package \
+            -v \
+            -f \
+            -m \
+            -M ./AndroidManifest.xml \
+            -I $ANDROID_HOME/platforms/android-23/android.jar \
+            -I ./weixin_660.apk \
+            -F ./dist/dist.apk
+
 echo
 echo "==================== FILES CREATED ==================== "
+
+
+cd ./dist
+
+mv dist.apk dist.zip
+unzip -o dist.zip -d .
+cp ../weixin_660.apk          weixin_process.apk
+
+aapt r weixin_process.apk \
+                            META-INF/MANIFEST.MF \
+                            AndroidManifest.xml \
+                            META-INF/COM_TENC.SF \
+                            META-INF/COM_TENC.RSA
+
+aapt a weixin_process.apk \
+                            AndroidManifest.xml
+
+
+jarsigner -verbose \
+          -sigalg SHA1withRSA \
+          -digestalg SHA1 \
+          -keystore ../opentown.keystore \
+  weixin_process.apk opentown
+
+zipalign -f \
+         -v 4 \
+  weixin_process.apk weixin_complete.apk
+
+cp weixin_complete.apk weixin_complete.zip
+unzip -o weixin_complete.zip -d verify_dir
